e7cbc713f047c8a88580da4ee94087f9e9478b5f
L3D <l3d@see-base.de>
2019-06-10 23:32:29 +0200

    create better startup

---
+++ roles/copy_files/files/display-setup.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-1 --off --output DisplayPort-0 --off --output DVI-1 --mode 1280x1024 --pos 0x0 --rotate left --output DVI-0 --off --output HDMI-0 --mode 1920x1200 --pos 1024x80 --rotate normal
